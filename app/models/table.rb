4cb6074091c585492255b145c30b8ae7827cc286
磯崎裕太 <yutaisozaki@isozakiyuutanoMacBook-puro.local>
2019-06-26 03:03:26 +0900

    [add] TableType enum

--- app/models/table.rb
+++ app/models/table.rb
@@ -2,5 +2,6 @@ class Table < ApplicationRecord
   has_many :imanomus, dependent: :destroy
   belongs_to :shop
 
+  enum type: { 'テーブル席': 0, 'カウンター席': 1, '個室': 2, '座敷': 3, 'ソファー席': 4}
   enum vacancy_status: { '空席あり': 0, '空席わずか': 1, '満席': 2}
 end
