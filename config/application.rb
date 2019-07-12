55ee5d159fc9f3962b2454909e4374fd7192c53b
磯崎裕太 <yutaisozaki@isozakiyuutanoMacBook-puro.local>
2019-07-12 23:38:23 +0900

    production環境でrunnerが走るように設定

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module IMANOMUMvpApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.autoload_paths += Dir["#{config.root}/lib"]
+    config.eager_load_paths += Dir["#{config.root}/lib/**/"]
     config.load_defaults 5.2
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
